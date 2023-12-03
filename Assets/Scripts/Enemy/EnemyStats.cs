5700ee25c1d7e3da4bd92ab223756b9a245a4b26
Ali Biner <102030146+AliBiner@users.noreply.github.com>
2023-12-03 23:20:37 +0300

    Enemy Hit Effect and Knockback

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -7,10 +7,18 @@ public class EnemyStats : MonoBehaviour
     public float maxHealth;
     private float currentHealth;
     public GameObject deathEffect;
+    public float timer;
+
+    HitEffect effect;
+    Rigidbody2D rb;
+    public float knockBackForceX, knockBackForceY;
+    public Transform player;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        effect = GetComponent<HitEffect>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,9 +28,25 @@ public class EnemyStats : MonoBehaviour
     }
     public void TakeDamage(float damage){
         currentHealth -= damage;
+       
+        if (player.position.x < transform.position.x)
+        {
+            rb.AddForce(new Vector2(knockBackForceX, knockBackForceY), ForceMode2D.Force);
+        }
+        else
+        {
+            rb.AddForce(new Vector2(-knockBackForceX, knockBackForceY), ForceMode2D.Force);
+        }
+        GetComponent<SpriteRenderer>().material = effect.white;
+        StartCoroutine(BackToNormal());
         if(currentHealth<=0){
             Instantiate(deathEffect, transform.position,transform.rotation);
             Destroy(gameObject);
         }
+    }
+
+    IEnumerator BackToNormal() {
+        yield return new WaitForSeconds(timer);
+        GetComponent<SpriteRenderer>().material = effect.original;
     }
 }
