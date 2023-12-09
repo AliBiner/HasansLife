a6469b61f6cdcfea49b2b9a5597df4f600e79ceb
Ali Biner <102030146+AliBiner@users.noreply.github.com>
2023-12-09 03:13:04 +0300

    Create Health, Mana Bar and Take Hit Anim

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float maxHealth;
     float currentHealth;
+    public Image healthBar;
+    bool isImmune;
+    public float immunityTime;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        anim=GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,18 +26,27 @@ public class PlayerHealth : MonoBehaviour
         {
             currentHealth = maxHealth;
         }
+        healthBar.fillAmount = currentHealth/100;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy"))
+        if (collision.CompareTag("Enemy") && !isImmune)
         {
             currentHealth -= collision.GetComponent<EnemyStats>().damage;
+            StartCoroutine(Immunity());
+            anim.SetTrigger("Hit");
             if (currentHealth<=0)
             {
                 currentHealth = 0;
                 Destroy(gameObject);
             }
         }
+    }
+
+    IEnumerator Immunity() { 
+        isImmune = true;
+        yield return new WaitForSeconds(immunityTime);
+        isImmune=false;
     }
 }
